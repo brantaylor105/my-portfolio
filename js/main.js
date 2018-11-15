6aee6d6166eb5f56ac18222cec72cf1aab3b3ef2
Brandon Taylor <brantaylor105@gmail.com>
2018-11-15 09:09:55 -0700

    Editing work examples

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ import ExampleWork from './example-work';
 
 const myWork = [
   {
-    'title': "Work Example",
+    'title': "Work Example1",
     'href': "https://example.com",
     'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
@@ -14,7 +14,7 @@ const myWork = [
     }
   },
   {
-    'title': "Work Example",
+    'title': "Work Example2",
     'href': "https://example.com",
     'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
@@ -24,7 +24,7 @@ const myWork = [
     }
   },
   {
-    'title': "Work Example",
+    'title': "Work Example3",
     'href': "https://example.com",
     'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'image': {
